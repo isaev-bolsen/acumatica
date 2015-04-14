aba24f66a92948b8f38f39f91af7e5479d212ca0
drBolsen <al_mamistov@mail.ru>
2015-04-14 21:11:36 +0300

    Terriable experiments with FireFox

--- acumatica/WebDriverFactory.cs
+++ acumatica/WebDriverFactory.cs
@@ -12,7 +12,13 @@ namespace acumatica
             BrowserName=BrowserName.ToLower();
             if ("chrome" == BrowserName) return new OpenQA.Selenium.Chrome.ChromeDriver();
             if ("safari" == BrowserName) return new OpenQA.Selenium.Safari.SafariDriver();
-            if ("firefox" == BrowserName || "fire fox" == BrowserName) return new OpenQA.Selenium.Firefox.FirefoxDriver();
+            if ("firefox" == BrowserName || "fire fox" == BrowserName)
+                {
+                //OpenQA.Selenium.Firefox.FirefoxProfile FP = new OpenQA.Selenium.Firefox.FirefoxProfile();
+                //FP.Clean();
+                //FP.SetPreference("app.update.auto", false);
+                return new OpenQA.Selenium.Firefox.FirefoxDriver();
+                }
             if ("ie" == BrowserName || "internet explorer" == BrowserName || "infernet exploder" == BrowserName) return new OpenQA.Selenium.IE.InternetExplorerDriver();
             throw new Exception("Unknown browser: " + BrowserName);
             }
